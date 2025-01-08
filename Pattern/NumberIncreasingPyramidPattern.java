e8ce28f1bba2c2b0caaf41ce7799fe35477f8157
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-08 10:44:20 +0530

    Create NumberIncreasingPyramidPattern.java

---
+++ Pattern/NumberIncreasingPyramidPattern.java
@@ -0,0 +1,35 @@
+/* Number-increasing Pyramid Pattern 
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+1 2 3 4 5 6 
+*/ 
+
+import java.util.*;
+
+public class GeeksForGeeks {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to handle number of columns
+            for (j = 1; j <= i; j++) {
+                // printing column values upto the row
+                // value.
+                System.out.print(j + " ");
+            }
+            // print new line for each row
+            System.out.println();
+        }
+    }
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
