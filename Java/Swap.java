5e2002f0ff0aa72159d5fca2087aaaeb8875b591
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-15 21:47:14 +0530

    Create Swap.java

---
+++ Java/Swap.java
@@ -0,0 +1,14 @@
+//Write a Java program to swap two numbers.
+
+import java.util.*;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+    }
+}
