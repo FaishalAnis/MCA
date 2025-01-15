903305e5370bc2ab25f0b9de7ea80a40a59cafa8
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-15 21:40:21 +0530

    Create HelloWorld.java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,9 @@
+//Write a Java Program to Print “HELLO JAVA”.
+
+import java.util.*;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("HELLO JAVA");
+    }
+}
