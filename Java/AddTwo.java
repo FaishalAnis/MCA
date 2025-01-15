55c0ae53ebd69dbcc721149c36b0bbde13e37b67
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-15 21:40:57 +0530

    Create AddTwo.java

---
+++ Java/AddTwo.java
@@ -0,0 +1,11 @@
+//Write a Java Program to add two numbers by declaring variables value.
+
+import java.util.*;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
