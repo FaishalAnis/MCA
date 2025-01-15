b4c78e62ced99be833bdfb013510911a9b5248eb
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-15 21:47:45 +0530

    Create LeapYear.java

---
+++ Java/LeapYear.java
@@ -0,0 +1,14 @@
+//Write a Java program to check whether a year is a leap year or not.
+
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2024;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
