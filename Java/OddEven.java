6e9d6d02a5ff2f61fda4414ee256dafc0ec0da77
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-15 21:45:00 +0530

    Create OddEven.java

---
+++ Java/OddEven.java
@@ -0,0 +1,14 @@
+//Write a Java Program to display whether a number is odd or even.
+
+import java.util.*;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int num = 4;
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
